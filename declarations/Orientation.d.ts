786e7b6c820771543a9022075fbfb550c74162ae
Jan Brockmeyer <jan.brockmeyer@mgm-tp.com>
2018-10-01 14:04:59 +0200

    chore(typings): remove declaration folder

--- declarations/Orientation.d.ts
+++
@@ -1,7 +0,0 @@
-declare class Orientation {
-    static HORZ: Orientation;
-    static VERT: Orientation;
-    static flip(from: Orientation): Orientation;
-    toString(): string;
-}
-export default Orientation;
--- declarations/model/Action.d.ts
+++
@@ -1,7 +0,0 @@
-import { JSMap } from "../Types";
-declare class Action {
-    type: string;
-    data: JSMap<any>;
-    constructor(type: string, data: JSMap<any>);
-}
-export default Action;
